268dbd59e1eb381a0681830b2b47065fd2d8990f
Sela <ansela@paypal.com>
2015-12-04 22:10:17 +0200

    batchInterval to be determined by pipeline runner

--- src/main/java/com/cloudera/dataflow/io/CreateStream.java
+++ src/main/java/com/cloudera/dataflow/io/CreateStream.java
@@ -34,24 +34,20 @@ public final class CreateStream<T> {
    * Define the input stream to create from queue.
    *
    * @param queuedValues  defines the input stream
-   * @param batchInterval Spark streaming batch interval
    * @param <T>           stream type
    * @return the queue that defines the input stream
    */
-  public static <T> QueuedValues<T> fromQueue(Iterable<Iterable<T>> queuedValues, Long
-          batchInterval) {
-    return new QueuedValues<>(queuedValues, batchInterval);
+  public static <T> QueuedValues<T> fromQueue(Iterable<Iterable<T>> queuedValues) {
+    return new QueuedValues<>(queuedValues);
   }
 
   public static final class QueuedValues<T> extends PTransform<PInput, PCollection<T>> {
 
     private final Iterable<Iterable<T>> queuedValues;
 
-    QueuedValues(Iterable<Iterable<T>> queuedValues, Long batchInterval) {
+    QueuedValues(Iterable<Iterable<T>> queuedValues) {
       Preconditions.checkNotNull(queuedValues,
               "need to set the queuedValues of an Create.QueuedValues transform");
-      Preconditions.checkNotNull(batchInterval,
-              "need to set the batchInterval of a SocketIO.Read transform");
       this.queuedValues = queuedValues;
     }
 
